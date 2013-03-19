654f1dd3378550eaa6d054cb6308cfc651d78724
Martial <martialou@gmail.com>
2013-03-19 20:18:24 +0100

    fix(templates): refresh Template entity after update

--- Entity/Template.php
+++ Entity/Template.php
@@ -39,7 +39,7 @@ class Template
     /**
      * @var string
      *
-     * @ORM\Column(name="filename", type="string", length=255)
+     * @ORM\Column(name="filename", type="string", length=255, nullable=true)
      */
     private $filename;
 
@@ -150,3 +150,4 @@ class Template
     }
 
 }
+
