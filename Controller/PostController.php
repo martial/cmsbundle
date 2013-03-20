00181b4713968ac94d384569ead3fb44fc8c0d7d
Martial <martialou@gmail.com>
2013-03-20 16:58:32 +0100

    cleaned controller and views

--- Controller/PostController.php
+++ Controller/PostController.php
@@ -14,20 +14,7 @@ use scrclub\CMSBundle\Form\PostType;
  */
 class PostController extends Controller
 {
-    /**
-     * Lists all Post entities.
-     *
-     */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getManager();
 
-        $entities = $em->getRepository('scrclubCMSBundle:Post')->findAll();
-
-        return $this->render('scrclubCMSBundle:Post:index.html.twig', array(
-            'entities' => $entities,
-        ));
-    }
 
     /**
      * Finds and displays a Post entity.
@@ -52,10 +39,6 @@ class PostController extends Controller
         //$query->andWhere("node.type = 'post'");
         $result = $query->getQuery()->getResult();
 
-        //echo count($result);
-
-        // wtf
-
 
 
         $rootTree = $repo->getRootNodes();
@@ -71,130 +54,4 @@ class PostController extends Controller
 
     }
 
-    /**
-     * Displays a form to create a new Post entity.
-     *
-     */
-    public function newAction()
-    {
-        $entity = new Post();
-        $form   = $this->createForm(new PostType(), $entity);
-
-        return $this->render('scrclubCMSBundle:Post:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        ));
-    }
-
-    /**
-     * Creates a new Post entity.
-     *
-     */
-    public function createAction(Request $request)
-    {
-        $entity  = new Post();
-        $form = $this->createForm(new PostType(), $entity);
-        $form->bind($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($entity);
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('backend_post_show', array('id' => $entity->getId())));
-        }
-
-        return $this->render('scrclubCMSBundle:Post:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        ));
-    }
-
-    /**
-     * Displays a form to edit an existing Post entity.
-     *
-     */
-    public function editAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('scrclubCMSBundle:Post')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Post entity.');
-        }
-
-        $editForm = $this->createForm(new PostType(), $entity);
-        $deleteForm = $this->createDeleteForm($id);
-
-        return $this->render('scrclubCMSBundle:Post:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Edits an existing Post entity.
-     *
-     */
-    public function updateAction(Request $request, $id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('scrclubCMSBundle:Post')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Post entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createForm(new PostType(), $entity);
-        $editForm->bind($request);
-
-        if ($editForm->isValid()) {
-            $em->persist($entity);
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('backend_post_edit', array('id' => $id)));
-        }
-
-        return $this->render('scrclubCMSBundle:Post:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Deletes a Post entity.
-     *
-     */
-    public function deleteAction(Request $request, $id)
-    {
-        $form = $this->createDeleteForm($id);
-        $form->bind($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('scrclubCMSBundle:Post')->find($id);
-
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find Post entity.');
-            }
-
-            $em->remove($entity);
-            $em->flush();
-        }
-
-        return $this->redirect($this->generateUrl('backend_post'));
-    }
-
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder(array('id' => $id))
-            ->add('id', 'hidden')
-            ->getForm()
-        ;
-    }
 }
