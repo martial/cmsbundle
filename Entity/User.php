e7718361f36f360b47b5d99f0d23048808a5366d
Martial <martialou@gmail.com>
2013-06-04 18:23:47 +0200

    fix(auth): adjust User entity and routing for FOSUserBundle upgrade

--- Entity/User.php
+++ Entity/User.php
@@ -3,7 +3,7 @@
 
     namespace scrclub\CMSBundle\Entity;
 
-    use FOS\UserBundle\Entity\User as BaseUser;
+    use FOS\UserBundle\Model\User as BaseUser;
     use Doctrine\ORM\Mapping as ORM;
 
     /**
