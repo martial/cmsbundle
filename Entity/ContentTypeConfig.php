38630d8e2ed6663f260d5c3b3decfe3c63ff9595
Martial <martialou@gmail.com>
2013-09-13 17:06:56 +0200

    added textTypes

---
+++ Entity/ContentTypeConfig.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace scrclub\CMSBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+
+use Gedmo\Translatable\Translatable;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+/**
+ * Category
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ * @ORM\Entity(repositoryClass="scrclub\CMSBundle\Entity\ContentTypeRepository")
+ * @ORM\InheritanceType("JOINED")
+ * @Gedmo\TranslationEntity(class="scrclub\CMSBundle\Entity\Translation\ContentTypeTranslation")
+ * @ORM\DiscriminatorColumn(name="discr", type="string")
+ * @ORM\DiscriminatorMap({"textContentType" = "TextContentType" })
+ */
+
+class ContentType
+{
+
+    public function __construct()
+    {
+
+        $this->translations = new ArrayCollection();
+
+    }
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription() {
+        return $this->description;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() {
+        return $this->name;
+    }
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=true)
+     *
+     */
+    protected $name;
+
+    /**
+     * @var string
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    protected $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=true)
+     *
+     */
+    protected $type;
+
+    /**
+     * @param string $type
+     */
+    public function setType($type) {
+        $this->type = $type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType() {
+        return $this->type;
+    }
+
+    /**
+     * @ORM\OneToMany(
+     *  targetEntity="scrclub\CMSBundle\Entity\Translation\NodeTranslation",
+     *  mappedBy="object",
+     *  cascade={"persist", "remove"}
+     * )
+     * @Assert\Valid(deep = true)
+     */
+    protected $translations;
+
+
+
+    /**
+     * Set translations
+     *
+     * @param ArrayCollection $translations
+     * @return Node
+     */
+    public function setTranslations($translations)
+    {
+        foreach ($translations as $translation) {
+            $translation->setObject($this);
+        }
+        $this->translations = $translations;
+        return $this;
+    }
+
+
+
+    /**
+     * Get translations
+     *
+     * @return ArrayCollection
+     */
+    public function getTranslations()
+    {
+        return $this->translations;
+    }
+
+    /**
+     * Add translation
+     *
+     * @param ProductTranslation
+     */
+    public function addTranslation($translation)
+    {
+        if ($translation->getContent()) {
+            $translation->setObject($this);
+            $this->translations->add($translation);
+        }
+    }
+
+
+
+    /**
+     * Remove translation
+     *
+     * @param ProductTranslation
+     */
+    public function removeTranslation($translation)
+    {
+        $this->translations->removeElement($translation);
+    }
+
+
+
+}
---
+++ Form/ContentConfigType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace scrclub\CMSBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ConfigType extends AbstractType
+{
+
+
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('sitename')
+            ->add('metakey')
+            ->add('metadescr')
+            ->add('gg_email', 'email')
+            ->add('gg_password', 'password',array("required" => false))
+            ->add('gg_analyticsid')
+            ->add('gs_apikey')
+            ->add('gs_sitetoken')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'scrclub\CMSBundle\Entity\Config'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'scrclub_cmsbundle_configtype';
+    }
+}
